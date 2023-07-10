d7e3f9de9fe87964d2c89b7167db07696915d98e
TreszkaiT <treszkaitom@gmail.com>
2023-07-10 14:08:53 +0200

    shared.module

---
+++ src/app/module/common/shared.module.ts
@@ -0,0 +1,49 @@
+// import { NgxPermissionsModule } from 'ngx-permissions';
+// import { ButtonModule } from 'primeng/button';
+// import { DropdownModule } from 'primeng/dropdown';
+// import { ImageModule } from 'primeng/image';
+// import { InputTextModule } from 'primeng/inputtext';
+// import { InputTextareaModule } from 'primeng/inputtextarea';
+// import { MenubarModule } from 'primeng/menubar';
+// import { MessagesModule } from 'primeng/messages';
+// import { TableModule } from 'primeng/table';
+// import { TabViewModule } from 'primeng/tabview';
+// import { ToolbarModule } from 'primeng/toolbar';
+
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+    exports: [
+        CommonModule,
+        // ButtonModule,
+        // DropdownModule,
+        FormsModule,
+        // ImageModule,
+        // InputTextareaModule,
+        // InputTextModule,
+        // MenubarModule,
+        // MessagesModule,
+        // NgxPermissionsModule,
+        // TableModule,
+        // TabViewModule,
+        // ToolbarModule,
+    ],
+    imports: [
+        CommonModule,
+        // ButtonModule,
+        // DropdownModule,
+        FormsModule,
+        // ImageModule,
+        // InputTextareaModule,
+        // InputTextModule,
+        // MenubarModule,
+        // MessagesModule,
+        // NgxPermissionsModule,
+        // TableModule,
+        // TabViewModule,
+        // ToolbarModule,
+    ],
+})
+export class SharedModule {}
