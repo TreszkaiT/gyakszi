dac30c89b69dcb0b86d774dd72ee46d2e4694b8a
TreszkaiT <treszkaitom@gmail.com>
2023-07-10 15:02:51 +0200

    ngprime and menubar

--- src/app/module/common/shared.module.ts
+++ src/app/module/common/shared.module.ts
@@ -1,14 +1,14 @@
 // import { NgxPermissionsModule } from 'ngx-permissions';
-// import { ButtonModule } from 'primeng/button';
-// import { DropdownModule } from 'primeng/dropdown';
-// import { ImageModule } from 'primeng/image';
-// import { InputTextModule } from 'primeng/inputtext';
-// import { InputTextareaModule } from 'primeng/inputtextarea';
-// import { MenubarModule } from 'primeng/menubar';
-// import { MessagesModule } from 'primeng/messages';
-// import { TableModule } from 'primeng/table';
-// import { TabViewModule } from 'primeng/tabview';
-// import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { ImageModule } from 'primeng/image';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { MenubarModule } from 'primeng/menubar';
+import { MessagesModule } from 'primeng/messages';
+import { TableModule } from 'primeng/table';
+import { TabViewModule } from 'primeng/tabview';
+import { ToolbarModule } from 'primeng/toolbar';
 
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
@@ -17,33 +17,33 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
     exports: [
         CommonModule,
-        // ButtonModule,
-        // DropdownModule,
+        ButtonModule,
+        DropdownModule,
         FormsModule,
-        // ImageModule,
-        // InputTextareaModule,
-        // InputTextModule,
-        // MenubarModule,
-        // MessagesModule,
+        ImageModule,
+        InputTextareaModule,
+        InputTextModule,
+        MenubarModule,
+        MessagesModule,
         // NgxPermissionsModule,
-        // TableModule,
-        // TabViewModule,
-        // ToolbarModule,
+        TableModule,
+        TabViewModule,
+        ToolbarModule,
     ],
     imports: [
         CommonModule,
-        // ButtonModule,
-        // DropdownModule,
+        ButtonModule,
+        DropdownModule,
         FormsModule,
-        // ImageModule,
-        // InputTextareaModule,
-        // InputTextModule,
-        // MenubarModule,
-        // MessagesModule,
+        ImageModule,
+        InputTextareaModule,
+        InputTextModule,
+        MenubarModule,
+        MessagesModule,
         // NgxPermissionsModule,
-        // TableModule,
-        // TabViewModule,
-        // ToolbarModule,
+        TableModule,
+        TabViewModule,
+        ToolbarModule,
     ],
 })
 export class SharedModule {}
